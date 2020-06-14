15b9ad86ed5a369bdfe8f29d6931f5c50d814d59
Rin Raeuber <mail@rin-raeuber.com>
2020-06-14 21:24:13 +0200

    Feature: Add color picker to profile.

--- src/components/portrait/index.tsx
+++ src/components/portrait/index.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ColorPicker from '../colorPicker';
 import './portrait.scss'
 
-const PORTRAIT_URL = "https://avatars1.githubusercontent.com/u/294558?s=460&u=5fb53f0e22df138553a9133baae7a25e6e918e35&v=4";
+const COLORS = ['#b6a1ce', '#a1cebc', '#90b2d6', '#c5c5c5'];
 
-const Portrait = () => 
-<div className='portraitWrapper'>
-  <img src={PORTRAIT_URL} className='portrait' alt="Portrait" />
-</div>
+const Portrait = () => {
+  const [background, setBackground] = useState();
+  const [colorPickerDisplayed, setColorPicker] = useState(false);
+
+  const showColorPicker = () => setColorPicker(true)
+  const hideColorPicker = () => setColorPicker(false);
+
+  const onColorSelected = ({ hex } : any) => {
+    setBackground(hex);
+    hideColorPicker();
+  };
+
+  return (
+    <div>
+      {colorPickerDisplayed && (
+        <ColorPicker
+          colors={COLORS} 
+          color={background || ''} 
+          onColorSelected={onColorSelected} 
+        />
+      )}
+      <button 
+        className="colorPickerTrigger" 
+        onClick={showColorPicker}
+      >
+        ✎ Change Background
+      </button>
+      <img 
+        src='/rin-cv-portrait.png' 
+        style={{ background }} 
+        className='portrait' 
+        alt="Portrait" 
+      />
+  </div>
+  )
+};
 
 export default Portrait;

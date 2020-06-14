36c78882b62d09322c42e22cb07c9d2447c109e1
Rin Raeuber <mail@rin-raeuber.com>
2020-06-14 21:27:08 +0200

    Feature: Get email from backend.

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Portrait from '../portrait';
 import './intro.scss'
 
-const Intro = () => 
+interface ContactInfo {
+  email?: string;
+}
+
+const Intro = ({ email }: ContactInfo) => 
 <div className='introWrapper'>
   <Portrait />
   <div className='aboutText'>
@@ -10,7 +14,7 @@ const Intro = () =>
     <p>My name is Rin.</p>
     <p>I care about UX and developer happiness, but I've also learnt to get something done quickly if needed.</p>
     <p>The last two years, I've worked fully remotely with a team distributed over at least five countries.</p>
-    <p>Got something I'd wanna check out? E-mail me at <mark>mail@rin-raeuber.com</mark>.</p>
+<p>Got something I'd wanna check out? E-mail me at <mark>{email}</mark>.</p>
     <img src="/rin-signature.png" width="100px" alt="Rin" />
   </div>
 </div>

ccaf5550c5b9410b9b7832ffb94ed6cc12419175
Rin Raeuber <mail@rin-raeuber.com>
2020-06-20 20:00:16 +0200

    Feature: Remove colorpicker from portrait.

--- src/components/portrait/index.tsx
+++ src/components/portrait/index.tsx
@@ -1,36 +1,10 @@
-import React, { useState } from 'react';
-import ColorPicker from '../colorPicker';
+import React from 'react';
 import './portrait.scss'
 
-const COLORS = ['#b6a1ce', '#a1cebc', '#90b2d6', '#c5c5c5'];
-
 const Portrait = () => {
-  const [background, setBackground] = useState();
-  const [colorPickerDisplayed, setColorPicker] = useState(false);
-
-  const showColorPicker = () => setColorPicker(true)
-  const hideColorPicker = () => setColorPicker(false);
-
-  const onColorSelected = ({ hex } : any) => {
-    setBackground(hex);
-    hideColorPicker();
-  };
 
   return (
-    <div className="portraitWrapper" style={{ background }} >
-      {colorPickerDisplayed && (
-        <ColorPicker
-          colors={COLORS} 
-          color={background || ''} 
-          onColorSelected={onColorSelected} 
-        />
-      )}
-      <button 
-        className="colorPickerTrigger" 
-        onClick={showColorPicker}
-      >
-        ✎ Change Color
-      </button>
+    <div className="portraitWrapper">
       <img 
         src='/rin-cv-portrait.png' 
         className='portrait' 

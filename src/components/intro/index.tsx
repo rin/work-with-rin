1352d6340d3d742033569e646ac6114f2d7401e5
Rin Raeuber <mail@rin-raeuber.com>
2020-06-21 22:33:28 +0200

    Chore: Update intro text.

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -10,11 +10,20 @@ const Intro = ({ email }: IntroProps) =>
 <div className='introWrapper'>
   <Portrait />
   <div className='aboutText'>
-    <p>Hello, fellow human.</p>
-    <p>My name is Rin.</p>
-    <p>I care about UX and developer happiness, but I've also learnt to get something done quickly if needed.</p>
-    <p>The last two years, I've worked fully remotely with a team distributed over at least five countries.</p>
-<p>Got something I'd wanna check out? E-mail me at <mark>{email}</mark>.</p>
+    <p>
+      Hello, fellow human.
+    </p>
+    <p>
+      My name is Rin. I enjoy coding and building things, and 
+      I like to use it to contribute to a better world.
+    </p>
+    <p>
+      The last two years, I've worked fully remotely with a team 
+      distributed over at least five countries. It’s how I do my best work.
+    </p>
+    <p>
+      Got something I'd wanna check out? E-mail me at <mark>{email}</mark>.
+    </p>
     <img src="/rin-signature.png" width="100px" alt="Rin" />
   </div>
 </div>
